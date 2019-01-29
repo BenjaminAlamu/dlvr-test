a20cd98c3a4235cb1c03b3f1e7f63cfc16cdcfb9
Benjamin Alamu <oluwaseunalamu@gmail.com>
2019-01-29 16:34:16 +0100

    Paginationn Implemented

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,24 +2,38 @@ let table = document.getElementById("table");
 
 
 let json_data = data;
+let displayed_data = json_data.slice(0, 10)
 printData()
+addButtons()
 
 function printData() {
     table.innerHTML = ""
-    for (let i = 0; i < json_data.length; i++) {
+    for (let i = 0; i < displayed_data.length; i++) {
         var row_data = ""
         row_data = '<tr>' +
-            ' <td>' + json_data[i].firstName + '</td>' +
-            ' <td>' + json_data[i].lastName + '</td>' +
-            ' <td>' + json_data[i].occupation + '</td>' +
-            ' <td>' + json_data[i].phoneNumber +
+            ' <td>' + displayed_data[i].firstName + '</td>' +
+            ' <td>' + displayed_data[i].lastName + '</td>' +
+            ' <td>' + displayed_data[i].occupation + '</td>' +
+            ' <td>' + displayed_data[i].phoneNumber +
             '</tr>';
         table.innerHTML += row_data
     }
 }
 
+function addButtons() {
+    let button = document.getElementById("buttons");
+    for (let i = 0; i < json_data.length; i += 10) {
+        let val = i / 10 + 1;
+        button.innerHTML += `<button onclick='trimArray(${i})'>${val}</button>`
+    }
+}
 
+function trimArray(index) {
+    console.log(index);
+    displayed_data = json_data.slice(index, index + 10)
+    printData();
 
+}
 
 function parentSort(value) {
 
